584ac9248538f2713f1213b16bdff299e8dec003
Raian de Andrades <raian.andrades@gmail.com>
2021-06-19 22:49:20 -0300

    Criando estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+    :root {
+        --background: #f0f2f5;
+        --red: #e52e4d;
+        --blue: #5429cc;
+
+        --blue-light: #6933ff;
+
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --background: #f0f2f5;
+        --shape: #ffffff;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1000px){
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px){
+            font-size: 87.7%;
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialised;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled]: {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
